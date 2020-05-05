0a7ff6111f064283ffb77794e7a2894063b535a6
ruk <ruk.lakhani@students.makeschool.com>
2020-05-05 12:39:27 -0700

    first commit

---
+++ stack.py
@@ -0,0 +1,112 @@
+#!python
+
+from linkedlist import LinkedList
+
+
+# Implement LinkedStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class LinkedStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new linked list to store the items
+        self.list = LinkedList()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # TODO: Check if empty
+        return self.list.is_empty()
+
+    def length(self):
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+        return self.list.length()
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [O(1) because the append function is O(1)]"""
+        # TODO: Push given item
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+        if self.is_empty():
+            return None
+        return self.list.tail.data
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [O(n) because we are deleting the tail]"""
+        # TODO: Remove and return top item, if any
+        if self.is_empty():
+            raise ValueError('empty :^(')
+        data = self.list.tail.data
+        self.list.delete(data)
+        return data
+
+
+# Implement ArrayStack below, then change the assignment at the bottom
+# to use this Stack implementation to verify it passes all tests
+class ArrayStack(object):
+
+    def __init__(self, iterable=None):
+        """Initialize this stack and push the given items, if any."""
+        # Initialize a new list (dynamic array) to store the items
+        self.list = list()
+        if iterable is not None:
+            for item in iterable:
+                self.push(item)
+
+    def __repr__(self):
+        """Return a string representation of this stack."""
+        return 'Stack({} items, top={})'.format(self.length(), self.peek())
+
+    def is_empty(self):
+        """Return True if this stack is empty, or False otherwise."""
+        # TODO: Check if empty
+        if len(self.list) == 0:
+            return True
+        return False
+
+    def length(self):
+        """Return the number of items in this stack."""
+        # TODO: Count number of items
+        return len(self.list)
+
+    def push(self, item):
+        """Insert the given item on the top of this stack.
+        Running time: O(???) – Why? [O(1) on average]"""
+        # TODO: Insert given item
+        self.list.append(item)
+
+    def peek(self):
+        """Return the item on the top of this stack without removing it,
+        or None if this stack is empty."""
+        # TODO: Return top item, if any
+        if self.is_empty():
+            return None
+        return self.list[-1]
+
+    def pop(self):
+        """Remove and return the item on the top of this stack,
+        or raise ValueError if this stack is empty.
+        Running time: O(???) – Why? [O(1) because it's python's built in function]"""
+        # TODO: Remove and return top item, if any
+        if self.is_empty():
+            raise ValueError
+        return self.list.pop()
+
+# Implement LinkedStack and ArrayStack above, then change the assignment below
+# to use each of your Stack implementations to verify they each pass all tests
+# Stack = LinkedStack
+Stack = ArrayStack
